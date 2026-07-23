087e62da98976bc93e591bf82f65b0cb6a353e8f
rbecks17 <rbecks@amazon.co.uk>
2026-07-23 16:18:45 +0100

    Update sim-t-auto-resolver.meta.js

--- sim-t-auto-resolver.meta.js
+++ sim-t-auto-resolver.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SIM-T Auto Resolver
 // @namespace    https://t.corp.amazon.com/
-// @version      1.33.0
+// @version      1.35.0
 // @updateURL    https://raw.githubusercontent.com/rbecks17/sim-t-auto-resolver/main/sim-t-auto-resolver.meta.js
 // @downloadURL  https://raw.githubusercontent.com/rbecks17/sim-t-auto-resolver/main/sim-t-auto-resolver.user.js
 // ==/UserScript==
